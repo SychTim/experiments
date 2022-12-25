b53431159f25fab44629a3ceaf0c70b4561cdaa4
SychTim <sychtim@gmail.com>
2022-12-25 23:49:17 +0100

    добавляет падение картинок без анимации

--- js/eat.js
+++ js/eat.js
@@ -9,36 +9,36 @@ let второйВыбор;
 стартИгры();
 
 function стартИгры() {
-    for (const box of ящики) {
-        const рандомноеЧисло = Math.random() * 6;
-         let адрессСсылки;
-         let кодКартинки;
+     for (const box of ящики) {
+          const рандомноеЧисло = Math.random() * 6;
+          let адрессСсылки;
+          let кодКартинки;
 
-        const картинка = box.firstChild;
+          const картинка = box.firstChild;
 
-        if (рандомноеЧисло >= 0 && рандомноеЧисло < 1) {
-             адрессСсылки = './images/eat/burger.jpg';
-             кодКартинки = 'bg';
-        }else if (рандомноеЧисло >= 1 && рандомноеЧисло < 2) {
-             адрессСсылки = './images/eat/butterbroad.jpg';
-             кодКартинки = 'bb';
-        }else if (рандомноеЧисло >= 2 && рандомноеЧисло < 3) {
-             адрессСсылки = './images/eat/hotdog.jpg';
-             кодКартинки = 'hd';
-        }else if (рандомноеЧисло >= 3 && рандомноеЧисло < 4) {
-             адрессСсылки = './images/eat/pizza.jpg';
-             кодКартинки = 'pz';
-        }else if (рандомноеЧисло >= 4 && рандомноеЧисло < 5) {
-             адрессСсылки = './images/eat/ponchik.jpg';
-             кодКартинки = 'ph';
-        }else {
-             адрессСсылки = './images/eat/potetofree.jpg';
-             кодКартинки = 'pf';
-         };
-         
-         картинка.src = адрессСсылки;
-         картинка.dataset.img = кодКартинки;
-    };
+          if (рандомноеЧисло >= 0 && рандомноеЧисло < 1) {
+               адрессСсылки = './images/eat/burger.jpg';
+               кодКартинки = 'bg';
+          } else if (рандомноеЧисло >= 1 && рандомноеЧисло < 2) {
+               адрессСсылки = './images/eat/butterbroad.jpg';
+               кодКартинки = 'bb';
+          } else if (рандомноеЧисло >= 2 && рандомноеЧисло < 3) {
+               адрессСсылки = './images/eat/hotdog.jpg';
+               кодКартинки = 'hd';
+          } else if (рандомноеЧисло >= 3 && рандомноеЧисло < 4) {
+               адрессСсылки = './images/eat/pizza.jpg';
+               кодКартинки = 'pz';
+          } else if (рандомноеЧисло >= 4 && рандомноеЧисло < 5) {
+               адрессСсылки = './images/eat/ponchik.jpg';
+               кодКартинки = 'ph';
+          } else {
+               адрессСсылки = './images/eat/potetofree.jpg';
+               кодКартинки = 'pf';
+          };
+
+          картинка.src = адрессСсылки;
+          картинка.dataset.img = кодКартинки;
+     };
 };
 
 игра.addEventListener('click', функцияПервогоВыбора);
@@ -74,7 +74,7 @@ function функцияВторогоВыбора(ивент) {
           второйВыбор.classList.add('moveTop2');
           setTimeout(сменаКартинок, 300);
      } else if (первыйВыбор.dataset.h - 1 == второйВыбор.dataset.h && первыйВыбор.dataset.r == второйВыбор.dataset.r) {
-          console.log('vlevo'); 
+          console.log('vlevo');
           первыйВыбор.classList.add('moveVlevo1');
           второйВыбор.classList.add('moveVlevo2');
           setTimeout(сменаКартинок, 300);
@@ -140,11 +140,87 @@ function проверкаСовпадений() {
           };
      };
 
+     for (const столбец of столбцы) {
+          let совпадение;
+          let счетчикСовпадений = 0;
+          const победныеКартинки = [];
+
+          for (const картинка of столбец) {
+               if (картинка.src === совпадение) {
+                    счетчикСовпадений += 1;
+                    победныеКартинки.push(картинка);
+               } else if (счетчикСовпадений < 2) {
+                    счетчикСовпадений = 0;
+                    победныеКартинки.length = 0;
+                    победныеКартинки.push(картинка);
+               } else if (счетчикСовпадений >= 2) {
+                    победныйКлич(победныеКартинки);
+                    return console.log('pobeda!!!', победныеКартинки);
+               };
+
+               совпадение = картинка.src;
+
+               console.log(счетчикСовпадений);
+          };
+
+          if (счетчикСовпадений >= 2) {
+               победныйКлич(победныеКартинки);
+               return console.log('победа', победныеКартинки);
+          };
+     };
+
      console.log('ne svezlo');
 };
 
 function победныйКлич(победныеКартинки) {
      победныеКартинки.forEach(картинка => {
           картинка.classList.add('WinCard');
+          картинка.dataset.img = "false";
      });
+
+     setTimeout(падениеКарт, 300);
+};
+
+function падениеКарт() {
+     for (let index = 0; index < 60; index += 1) {
+          const падающаяКарта = document.querySelector('[data-img="false"]');
+          if (падающаяКарта === null) {
+               return проверкаСовпадений();
+          };
+
+          const номерРяда = падающаяКарта.dataset.r;
+          const номерСтолба = падающаяКарта.dataset.h;
+          console.log(падающаяКарта);
+          if (номерРяда === '1') {
+               const рандомноеЧисло = Math.random() * 6;
+
+               if (рандомноеЧисло >= 0 && рандомноеЧисло < 1) {
+                    падающаяКарта.src = './images/eat/burger.jpg';
+               } else if (рандомноеЧисло >= 1 && рандомноеЧисло < 2) {
+                    падающаяКарта.src = './images/eat/butterbroad.jpg';
+               } else if (рандомноеЧисло >= 2 && рандомноеЧисло < 3) {
+                    падающаяКарта.src = './images/eat/hotdog.jpg';
+               } else if (рандомноеЧисло >= 3 && рандомноеЧисло < 4) {
+                    падающаяКарта.src = './images/eat/pizza.jpg';
+               } else if (рандомноеЧисло >= 4 && рандомноеЧисло < 5) {
+                    падающаяКарта.src = './images/eat/ponchik.jpg';
+               } else {
+                    падающаяКарта.src = './images/eat/potetofree.jpg';
+               };
+
+               падающаяКарта.classList.remove('WinCard');
+               
+               падающаяКарта.dataset.img = "true";
+
+               console.log("верхняя");
+
+
+          } else {
+               const верхняяКарта = document.querySelector(`[data-r="${Number(номерРяда) - 1}"][data-h="${номерСтолба}"]`);
+               падающаяКарта.classList.remove('WinCard');
+               падающаяКарта.src = верхняяКарта.src;
+               падающаяКарта.dataset.img = "true";
+               верхняяКарта.dataset.img = "false";
+          };
+     };
 };
